bf7c4659c5b753ef6ea8fe4e6b3dd9fd3c01ea34
Victor Roman <Romantech91>
2024-09-01 00:44:43 -0700

    header.jsx completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function Header() {
+  return (
+    <header>
+      <h1>Victor A. Roman</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
